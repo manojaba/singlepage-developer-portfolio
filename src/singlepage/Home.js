8a49c43abb8d8d4c7a6842bebc2e28e5ef1a586e
Manoj Baskar <147849968+manojaba@users.noreply.github.com>
2024-11-27 13:49:27 +0530

    'commit

--- src/singlepage/Home.js
+++ src/singlepage/Home.js
@@ -8,9 +8,11 @@ function Home() {
         <div className='border border-black bg-blue-100 '>
 
 
-            <div className=' border-green-300 flex flex-col items-center gap-'>
-                <Topbar />
-                <img className='max-w-[174px] grayscale bg-[#242424]   ' src='/assets/images/manoj-zoom.png' alt='profile pic'></img>
+            <div className='flex justify-center'>
+                <div className=' border-green-300 bg-[#242424] flex flex-col items-center w-min'>
+                    <Topbar />
+                    <img className='max-w-[174px] grayscale    ' src='/assets/images/manoj-zoom.png' alt='profile pic'></img>
+                </div>
             </div>
 
 
